1bac59c7137c8ab6ea99d55eea247a2728d8f2cd
Jose Chavez <jose.chavez@dialexa.com>
2024-07-31 10:37:29 -0700

    Added indent size of 4

--- app/src/main/java/com/rangerforce/concurrency/App.java
+++ app/src/main/java/com/rangerforce/concurrency/App.java
@@ -2,11 +2,11 @@
 package com.rangerforce.concurrency;
 
 public class App {
-  public String getGreeting() {
-    return "Hello World!";
-  }
+    public String getGreeting() {
+        return "Hello World!";
+    }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
-  }
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
 }
--- app/src/main/java/com/rangerforce/concurrency/SetNumberTask.java
+++ app/src/main/java/com/rangerforce/concurrency/SetNumberTask.java
@@ -3,20 +3,20 @@ package com.rangerforce.concurrency;
 
 public class SetNumberTask implements Runnable {
 
-  private volatile int number;
-  private final int finalNumber;
+    private volatile int number;
+    private final int finalNumber;
 
-  public SetNumberTask(int finalNumber) {
-    this.number = 0;
-    this.finalNumber = finalNumber;
-  }
+    public SetNumberTask(int finalNumber) {
+        this.number = 0;
+        this.finalNumber = finalNumber;
+    }
 
-  @Override
-  public void run() {
-    number = finalNumber;
-  }
+    @Override
+    public void run() {
+        number = finalNumber;
+    }
 
-  public int getNumber() {
-    return number;
-  }
+    public int getNumber() {
+        return number;
+    }
 }
